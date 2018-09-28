29ad7c3cee3fc3e3df8f178022163e98478bdae4
Flo <ffallenbuechel@aol.com>
2018-09-28 03:32:04 +0200

    Changed box counting alg, still needs testing

--- entropy.py
+++ entropy.py
@@ -8,39 +8,75 @@ from tqdm import trange
 fsize = 1024
 ssize = 512
 
-y, sr = librosa.load("/home/flo/IAML/fma_small/000/000002.mp3", mono=True)
-print(y.shape)
+y, sr = librosa.load("/home/flo/IAML/fma_small/000/000140.mp3", duration=30.0, mono=True)
+lenY = y.shape[0]
+lenCut = lenY-(lenY%ssize)
+print(lenY, lenCut)
 
-framelist = []
-for i in range(0, (y.shape[0]-fsize), ssize):
-    energy = 0
-    for x in range(fsize):
-        energy += y[i+x]**2
-    framelist.append(energy)
+energy = (y[:lenCut].reshape(-1,ssize))**2
+energylist = np.concatenate((energy[:-1], energy[1:]), axis=1)
+
+framelist = energylist.sum(axis=1)
+
+p = energylist / framelist[:,None]
+entropy = -(p * np.nan_to_num(np.log2(p))).sum(axis=1)
+
+entdif = entropy[:-1] - entropy[1:]
+med = max(entdif.min(), entdif.max(), key=abs)
+
+BlockSize = []
+BlockSize.append(1)
+NumBox = []
+NumBox.append(1)
+
+for i in range(0, lenY-1):
+    NumBox[0] += 1 + abs(y[i]-y[i+1])
+
+NumCols = int(lenY / 2)
+UpperValue = [None] * NumCols
+LowerValue = [None] * NumCols
+
+for i in range(0, NumCols):
+    UpperValue[i] = max(y[2*i], y[2*i+1])
+    LowerValue[i] = min(y[2*i], y[2*i+1])
+
+maxScale = int(np.floor(np.log(lenY) / np.log(2)))
+
+for scale in range(1, maxScale):
+    dummy = 0
+    BlockSize.append(BlockSize[scale-1]*2)
+    for i in range(0, NumCols-1):
+        dummy += UpperValue[i] - LowerValue[i] + 1
+        if UpperValue[i] < LowerValue[i+1]:
+            dummy += LowerValue[i+1] - UpperValue[i]
+        if LowerValue[i] > UpperValue[i+1]:
+            dummy += LowerValue[i] - UpperValue[i+1]
     
-entropy = []
-for n, i in enumerate(range(0, (y.shape[0]-fsize), ssize)):
-    entr = 0
-    for x in range(fsize):
-        if(y[i+x] != 0):
-            p = y[i+x]**2 / framelist[n]
-            entr += p * np.log2(p)
-    entropy.append(-entr)
+    NumBox.append(dummy/BlockSize[scale])
+    NumCols = int(NumCols / 2)
+    for i in range (0, NumCols):
+        UpperValue[i] = max(UpperValue[2*i], UpperValue[2*i+1])
+        LowerValue[i] = min(LowerValue[2*i], LowerValue[2*i+1])
 
-med = 0
-for i in range(len(entropy)-1):
-    dif = entropy[i] - entropy[i+1]
-    if(abs(dif) > abs(med)):
-        med = dif
+BlockSize = np.array(BlockSize)
+NumBox = np.array(NumBox)
+print("SHAPES")
+print(BlockSize)
+print(NumBox)
 
-smax = int(y.shape[0] / 2)
+N = np.log(NumBox)
+R = np.log(1/BlockSize)
+m = np.linalg.lstsq(R[:,None],N)
+print(m[0][0])
+
+
+""" smax = int(y.shape[0] / 2)
 N = []
 R = []
 for s in trange(2, smax):
-    lenY = y.shape[0]
+    cutS = lenY - (lenY%s)
     r = s / lenY
-    nra = 0
-    frac = y[:lenY-(lenY%s)].reshape(-1,s)
+    frac = y[:cutS].reshape(-1,s)
     subMax = np.max(frac, axis=1)
     subMin = np.min(frac, axis=1)
     nr = ((subMax - subMin) + 1)/s
@@ -50,15 +86,12 @@ for s in trange(2, smax):
 
 N = np.log(N)
 R = np.log(1/np.array(R))
-m = np.linalg.lstsq(R[:,None],N)
-
-entropy = np.array(entropy)
+m = np.linalg.lstsq(R[:,None],N) """
 
 avg = np.average(entropy)
 std = np.std(entropy)
 mxe = np.max(entropy)
 mne = np.min(entropy)
-frd = m[0]
+frd = m[0][0]
 
 print(avg, std, mxe, mne, med, frd)
-print("Dim: {}".format(m))
