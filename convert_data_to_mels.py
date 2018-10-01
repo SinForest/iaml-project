b8b202b892a67cc4d5f3c8f458297606cdedc8e1
Patrick Dammann <patrick@mathphys.stura.uni-heidelberg.de>
2018-10-01 19:40:07 +0200

    bugfix

--- convert_data_to_mels.py
+++ convert_data_to_mels.py
@@ -11,21 +11,29 @@ OUTPATH = "./melsset.ln"
 def save_mel(paths):
     
     inpath, outpath = paths
-
-    song, sr = librosa.load(inpath, mono=True)
-
-    n_fft = 2**11         # shortest human-disting. sound (music)
-    hop_length = 2**10    # => 50% overlap of frames
-    n_mels = 128
-
-    if len(song) < n_fft:
-        return
-
-    X = librosa.feature.melspectrogram(song, sr=sr, n_fft=n_fft, hop_length=hop_length)
-    if not os.path.isdir(os.path.dirname(outpath)):
-        os.makedirs(os.path.dirname(outpath))
     
-    np.save(outpath, X.astype(np.float32))
+    try:
+
+        song, sr = librosa.load(inpath, mono=True)
+
+        n_fft = 2**11         # shortest human-disting. sound (music)
+        hop_length = 2**10    # => 50% overlap of frames
+        n_mels = 128
+
+        if len(song) < n_fft:
+            return
+
+        X = librosa.feature.melspectrogram(song, sr=sr, n_fft=n_fft, hop_length=hop_length)
+        if not os.path.isdir(os.path.dirname(outpath)):
+            os.makedirs(os.path.dirname(outpath))
+        
+        np.save(outpath, X.astype(np.float32))
+        
+    except Exception as e:
+        
+        print(e)
+        return
+        
 
     return
 
