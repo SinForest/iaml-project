f6e26b29f4fadf26ed5314c7d0424ef043b1bdd6
Flo <ffallenbuechel@aol.com>
2018-10-06 05:41:44 +0200

    Genre distribution is imbalanced, wrote script to calculate genre distributions added parameter to entropyset to filter empty genre trained with tuned hyperparameters

--- entrset.py
+++ entrset.py
@@ -8,15 +8,24 @@ import numpy as np
 
 class EntropyDataset(Dataset):
 
-    def __init__(self, path="./entrset.ln"):
+    def __init__(self, path="./entrset.ln", FILTER=True):
         self.ent = np.load(os.path.join(path, 'entropy.npy'))
         print(self.ent.shape)
         self.lbl = np.load(os.path.join(path, 'labels.npy'))
         print(self.lbl.shape)
+
+        if FILTER:
+            self.ent = np.array(self.ent[self.lbl != 0])
+            self.lbl = np.array(self.lbl[self.lbl != 0])
+            self.lbl = self.lbl - 1
+            print(self.ent.shape)
+            print(self.lbl.shape)
+
         classes = set()
         for x in self.lbl:
             classes.add(x)
         self.n_classes = len(classes)
+        print(self.n_classes)
 
     def __getitem__(self, idx):
         X = self.ent[idx]
@@ -27,7 +36,7 @@ class EntropyDataset(Dataset):
     def __len__(self):
         return self.ent.shape[0]
 
-    def getsplit(self, val_size=0.2, shuffle=False):
+    def getsplit(self, val_size=0.2, shuffle=True):
         seed = 666 #such evil
         datasize = self.__len__()
         indices = list(range(datasize))
@@ -40,3 +49,15 @@ class EntropyDataset(Dataset):
         valid_set = Subset(self, val_indices)
 
         return train_set, valid_set
+
+    def getindices(self, val_size=0.2, shuffle=True):
+        seed = 666 #such evil
+        datasize = self.__len__()
+        indices = list(range(datasize))
+        split = int(np.floor(val_size * datasize))
+        if shuffle:
+            np.random.seed(seed)
+            np.random.shuffle(indices)
+        train_indices, val_indices = indices[split:], indices[:split]
+
+        return train_indices, val_indices
