acf97e196f8616d5a359b81f56ccfca08c0ade69
Patrick Dammann <github@thesinforest.eu>
2018-10-06 02:24:59 +0200

    training now logging

--- cnn/train.py
+++ cnn/train.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
 from itertools import count
+import pickle
 
 # my imports:
 sys.path.append("../")
@@ -18,11 +19,11 @@ _XX = "\033[0m"
 
 # CONST:
 IPATH        = "../melsset.ln"
-BATCH_SIZE   = 32
-N_PROC       = 32
+BATCH_SIZE   = 64
+N_PROC       = 16
 CUDA_DEVICE  = 0 #NOCUDA
 MEL_SEG_SIZE = 512 # ~25sec
-LOG_COUNT    = 20
+LOG_COUNT    = 100
 
 print("### creating dataset ###")
 dset = SoundfileDataset("../all_metadata.p", IPATH, seg_size=30, out_type='pre_mel', mel_seg_size=MEL_SEG_SIZE, verbose=True)
@@ -41,11 +42,13 @@ opti  = torch.optim.RMSprop(model.parameters())
 sched = torch.optim.lr_scheduler.ReduceLROnPlateau(opti, patience=3, factor=0.33, verbose=True, mode='max')
 
 print("### starting train loop ###")
+log = {}
 
 for epoch in count(1):
 
     abs_prec = 0
     losses = []
+    log[epoch] = {}
     model.train()
     for i, (X, y) in enumerate(tqdm(tloader, desc=f'epoch {epoch}'), 1):
 
@@ -66,6 +69,9 @@ for epoch in count(1):
 
         if i % LOG_COUNT == 0:
             tqdm.write(f"{_CB}B[{i:>4}/{len(tloader)}]{_XX}: r.prec: {(abs_prec * 100) / (BATCH_SIZE*i):>2.2f}; loss: {sum(losses)/len(losses):>2.3f}")
+            log[epoch][i] = {"prec": (abs_prec * 100) / (BATCH_SIZE*i), "loss": sum(losses)/len(losses)}
+
+            pickle.dump(log, open("./logs.p"))
         
         
     
@@ -86,4 +92,6 @@ for epoch in count(1):
     prec = abs_prec / (BATCH_SIZE * len(vloader))
     tqdm.write(f"{_CG}VALID[{epoch}]{_XX}: r.prec: {prec * 100:>2.2f}; loss: {sum(losses)/len(losses):>2.3f}")
     sched.step(prec)
+    log[epoch]["val"] = {"prec": (abs_prec * 100) / (BATCH_SIZE*i), "loss": sum(losses)/len(losses), "sched":sched.num_bad_epochs==0 and epoch!=1}
+    pickle.dump(log, open("./logs.p"))
     torch.save(model, f"./model_E{i}_P{int(prec * 1000)}.t")
